2c54399fd1a1990441bb31f7610d3947696fbb5e
waynerferreira <75390132+waynerferreira@users.noreply.github.com>
2021-08-31 19:38:27 -0300

    key

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
     backend "s3" {
       bucket = "waynerbucktohio"
     ##  dynamodb_table = "terraform-state-lock-dynamo"
-      key = "terraform-teste.tfstate"
+      key = {${{ secrets.AWS_ACCESS_KEY_ID }},${{ secrets.AWS_SECRET_ACCESS_KEY }}}
       region = "us-east-2"
       #encrypted = true
     }
