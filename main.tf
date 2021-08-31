e127c6584a87a28bd976aaa3c77c0a4a0015ffb0
waynerferreira <75390132+waynerferreira@users.noreply.github.com>
2021-08-31 19:16:37 -0300

    create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "us-east-2"
+    version = "~> 3.0"
+  }
+terraform {
+    backend "s3" {
+      bucket = "waynerbucktohio"
+    #  dynamodb_table = "terraform-state-lock-dynamo"
+     # key = "terraform-teste.tfstate"
+      region = "us-east-2"
+      encrypted = true
+    }
+}
